0d941c9aa996ca5b5d5cc0d25cccb4cdee7ef353
Lorena Morales <Lorenarm.999@gmail.com>
2022-09-27 20:18:48 -0600

    2nd index.js added to routes file

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,10 @@
 //require express
 const router = require('express').Router();
+const apiRputes = require('./api');
+const homeRoutes = require('./home-routes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRputes);
 
 //module.exports
 module.exports = router;
