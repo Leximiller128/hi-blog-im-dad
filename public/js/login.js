5577b345c75ba17672db400b84fd056b174df852
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-28 16:22:29 -0600

    working on login.js: if email and password

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,17 @@
 //start with const sucka, and event.preventDefault
 const loginHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
+  //collect the stuff from login form
+  const emailAddress = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trime();
+};
+
+if (email && password) {
+    //make sure the below route is correct
+  const response = await fetch("/controllers/api/user-routes.js", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: { "Content-Type": "application/json" },
+  });
 }
