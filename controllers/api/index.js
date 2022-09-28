22732e22ac677e0fb913de675569739e9c9af53f
Lorena Morales <Lorenarm.999@gmail.com>
2022-09-27 19:38:38 -0600

    index.js file added to routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,12 @@
 //require express
 const router = require('express').Router();
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
+const comments = require('./comment');
+
+router.use('./users', userRoutes);
+router.use('./posts', postRoutes);
+router.use('./reactions', comments);
 
 //module.exports
 module.exports = router;
