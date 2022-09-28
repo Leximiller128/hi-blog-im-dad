9f4634587df72c67fe3a42f789530edec3c74cff
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-09-28 16:24:37 -0600

    started reaction.js,added comments to helpers.js

---
+++ models/Reaction.js
@@ -0,0 +1,13 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Reaction extends Model {}
+
+Reaction.init(
+  {
+    id: DataTypes.INTEGER,
+    allowNull: true
+  }
+)
+
+// there is probably more that goes in here, will do more research. 
