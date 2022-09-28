2cc7d3094c47279be5f69face3870da5ddec40ab
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-27 19:47:21 -0600

    commented out incorrect? code and imported models: index.js in model folder

--- models/index.js
+++ models/index.js
@@ -1,10 +1,17 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
+// const path = require("path");
+// const express = require("express");
+// const session = require("express-session");
+// const exphbs = require("express-handlebars");
 // const helpers = require('./utils/helpers');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+//import models
+const Post = require('./Post')
+const Reaction = require('./Reaction')
+const User = require('./User')
 
-const sequelize = require("./config/config");
+
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// const sequelize = require("./config/config");
