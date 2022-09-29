84f4c548f0014606d07cde5a79da8f74fcc70a53
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-29 13:13:18 -0600

    logout.js complete with route question

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+// const { response } = require("express");
+
+const logout = async function () {
+  //check route here
+  const response = await fetch("/controllers/api", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Logout Failed");
+  }
+};
+
+document.querySelector('#logout-button').addEventListener('click', logout);
