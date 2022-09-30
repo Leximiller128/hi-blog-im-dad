b65e2caef1ae08b6278da4e6e6b65bc7d30e8f2a
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-29 18:54:57 -0600

    created signup file and moved signup code from login

--- public/js/login.js
+++ public/js/login.js
@@ -27,34 +27,5 @@ const loginHandler = async (event) => {
   }
 };
 
-//sign up block: should it have its own page?
-const signupHandler = async (event) => {
-  event.preventDefault();
-
-  // const name = document.querySelector("#username-input-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document
-    .querySelector("#password-input-signup")
-    .value.trim();
-
-  //is below path correct?
-  const response = await fetch("/controllers/api/user-routes.js", {
-    method: "POST",
-    body: JSON.stringify({
-      email: email.value,
-      password: password.value,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
-
-  if (response.ok) {
-    document.location.replace("/public/js/profile.js");
-  } else {
-    alert(response.statusText);
-  }
-};
-
 //query selctors
 document.querySelector("login-form").addEventListener("submit", loginHandler);
-
-document.querySelector("signup-form").addEventListener("submit", signupHandler);
---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+//sign up block: should it have its own page?
+const signupHandler = async (event) => {
+  event.preventDefault();
+
+  // const name = document.querySelector("#username-input-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document
+    .querySelector("#password-input-signup")
+    .value.trim();
+
+  //is below path correct?
+  const response = await fetch("/controllers/api/user-routes.js", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/public/js/profile.js");
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector("signup-form").addEventListener("submit", signupHandler);
