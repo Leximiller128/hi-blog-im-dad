d549708189e62ee68ca7c1d1938f01d4dd043573
Lorena Morales <Lorenarm.999@gmail.com>
2022-09-29 19:01:09 -0600

    removed comment.js and added start to dashboard

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,10 @@
 const router = require('express').Router();
 const postRoutes = require('./post-routes');
 const userRoutes = require('./user-routes');
-const comments = require('./comment');
+
 
 router.use('./users', userRoutes);
 router.use('./posts', postRoutes);
-router.use('./reactions', comments);
 
 //module.exports
 module.exports = router;
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,27 @@
 //require express
 const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
 
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        where: {
+          userId: req.session.userId,
+        },
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('all-posts-admin', {
+        layout: 'dashboard',
+        posts,
+      });
+    } catch (err) {
+      res.redirect('login');
+    }
+  });
 //module.exports
 module.exports = router;
 
