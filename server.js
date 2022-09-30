4fb8b303b39107750bcbadad4f292d866c6d6ea9
Lorena Morales <Lorenarm.999@gmail.com>
2022-09-29 19:46:19 -0600

    got npm start to work again

--- server.js
+++ server.js
@@ -8,24 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/config');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {
-//     maxAge: 300000,
-//     httpOnly: true,
-//     secure: false,
-//     sameSite: 'strict',
-//   },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-
-// app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
