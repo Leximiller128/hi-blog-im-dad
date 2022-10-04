96aad3be47be6bc91552382bfa172f1e26f4062e
Lorena Morales <Lorenarm.999@gmail.com>
2022-10-03 18:01:56 -0600

    adding routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,49 @@
-//require express
 const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
 
-//module.exports
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('dashboard', {
+      layout: 'main',
+      posts,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+router.get('/new', withAuth, (req, res) => {
+  res.render('makePost', {
+    layout: 'main',
+  });
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('edit-post', {
+        layout: 'main',
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
 module.exports = router;
