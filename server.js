b7567b9c1de5e0c2f903444b252335f4ff8245ac
Lorena Morales <Lorenarm.999@gmail.com>
2022-10-04 19:13:07 -0600

    force true

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.use(require("./controllers/"));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+  sequelize.sync({ force: true });
 });
